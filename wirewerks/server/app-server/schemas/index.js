bbd5635355acaf5a4534149093cc1cea1350b94f
Luc Vo Van <luc@unity3d.com>
2016-08-21 18:55:20 -0400

    adding fineprints

--- wirewerks/server/app-server/schemas/index.js
+++ wirewerks/server/app-server/schemas/index.js
@@ -16,6 +16,7 @@ class Product
 		this.partGroups = []; //possible zero
 		this.icon = "section8.svg";
 		this.section = 8;
+		this.finePrints = "* If more than one connector type, then place in alphabetical order, i.e. LC before SC, to avoid duplicate of part number creation for the same product. ** By default, cable assemblies with MPO connectors are manufactured with a Type A configuration. If an assembly is connectorized on both sides with an MPO connector, configuration types cannot be mixed. Examples: If a Type A is chosen on one end, the other end MUST also be of Type A configuration. *** If a decimal is required, add “D00” right after the non-decimal value and without leaving any space between. The “00” in “D00” must be replaced by the required value. Example: an assembly with an overall length of 105.55 meters shall read as 105D55."
 	}
 };
 
@@ -34,6 +35,7 @@ class PartCategory
 		this.title = "FIBER TYPE";
 		this.type = "A";  //this should tell us the colour needed.
 		this.parts = [];
+		this.finePrint = "*";
 	}
 };
 
@@ -43,7 +45,7 @@ class Part
 	{
 		this.description = "";
 		this.shortDescription = "";
-		this.finePrint = "example: if more than one blha blah";
+		this.finePrint = "**";
 		this.value = "1";
 		this.attributes = [];
 	}
--- wirewerks/tools/extractor/extract2.py
+++ wirewerks/tools/extractor/extract2.py
@@ -8,6 +8,10 @@ import os
 this creates a PartGroup json object from a csv file
 '''
 
+def extractFinePrint(value):
+    return len(value.split("*"))-1
+
+
 if len(sys.argv) != 2:
     print "usage extact2.py <file.csv>"
     quit()
@@ -24,12 +28,21 @@ partCategory = {}
 partCategory['title']=""
 partCategory['type']=""
 partCategory['parts'] = []
+partCategory['finePrint']=""
 
 f = open(filename, 'r')
 mylist = f.read().splitlines()
 
 #first value is title and second is type
-partCategory['title'] = mylist[0]
+
+finePrintNumber = extractFinePrint(mylist[0])
+partCategory['finePrint'] = '*'*finePrintNumber
+if finePrintNumber > 0 :
+    partCategory['title'] = mylist[0][:-finePrintNumber]
+else:
+    partCategory['title'] = mylist[0]
+
+
 partCategory['type'] = mylist[1]
 
 
@@ -47,11 +60,22 @@ for i in xrange(3,len(mylist)):
         part['shortDescription'] = ""
         part['finePrint'] = ""
         part['attributes'] = []
+
+        finePrintNumber = extractFinePrint(mylist[i])
+        part['finePrint'] = '*'*finePrintNumber
+        if finePrintNumber > 0 :
+            part['value'] = mylist[i][:-finePrintNumber]
+        else:
+            part['value'] = mylist[i]
+
     elif mylist[i] != "":
         #it's an attribute
         attribute = {}
         attribute['label'] = mylist[i]
-        attribute['bold'] = False
+        if mylist[i] == "Bend Insensitive":
+            attribute['bold'] = True
+        else:
+            attribute['bold'] = False
         part['attributes'].append(attribute)
     else:
         #finish
