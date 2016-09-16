d1ab1f364c6db7e0b3666563a96feed3e3405ee3
Mathieu Rivest <mathieu.rivest@yahoo.ca>
2016-09-15 22:21:41 -0400

    Prod fix

--- setup_production.sh
+++ setup_production.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 sh setup.sh
+
 cd wirewerks/client/ui
+
+# Copying common since links don't work on heroku build (EPERM error)
+echo *** Copying Common ***
+cd -R ../../common ./src/common
+
+echo *** Creating Distribution Builld ***
 node index.js
