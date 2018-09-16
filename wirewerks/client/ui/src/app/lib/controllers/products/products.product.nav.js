d4de846bd8d443a8491e700f879816d6d4af5be1
charles attendu <charles.attendu@gmail.com>
2018-09-16 17:58:05 -0400

    Properly display nbsp in nav and modal

--- wirewerks/client/ui/src/app/lib/controllers/products/products.product.nav.js
+++ wirewerks/client/ui/src/app/lib/controllers/products/products.product.nav.js
@@ -1,4 +1,8 @@
-var productsProductNavController = function (section, product, $scope) {
+var productsProductNavController = function (section, product, $scope, $sce) {
     $scope.section = section;
     $scope.product = product;
+
+    $scope.getAsHtml = function (value) {
+        return $sce.trustAsHtml(value) || $sce.trustAsHtml('&nbsp');
+    };
 };
